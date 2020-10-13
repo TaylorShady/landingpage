393df9d3dcbd0e273820cb5de091ef80d483cee0
taylorshady <taylorshadydesigner@gmail.com>
2020-10-12 22:45:54 -0300

    actualized

---
+++ js/scriptaction.js
@@ -0,0 +1,17 @@
+
+
+$(".bnt-menu-responsive").click(function(){
+    $(".menu-responsivo").fadeIn();
+
+});
+
+
+$(".bnt-menu-responsive-close").click(function(){
+    $(".menu-responsivo").fadeOut();
+
+});
+
+
+
+
+  
